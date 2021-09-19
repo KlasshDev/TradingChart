0d179a0c615bc126b8b6d34b37a9ef0a7695a795
Klassh <klasshdev@gmail.com>
2021-09-19 08:25:08 -0600

    Refactored to allow for user to input stock ticker

--- tradingchart.py
+++ tradingchart.py
@@ -13,89 +13,6 @@ from plotly.subplots import make_subplots
 import pandas as pd
 from datetime import datetime
 
-stock = "SPY"
-df = web.DataReader(stock, data_source='yahoo', start='01-01-2019')
-#------------------------------------------------------------------------------
-#   - Different Traces to add to charts -
-#------------------------------------------------------------------------------
-
-# Main candle stick chart
-Trace1 = {
-    'x': df.index,
-    'open': df.Open,
-    'close': df.Close,
-    'high': df.High,
-    'low': df.Low,
-    'type': 'candlestick',
-    'name': stock,
-    'showlegend':True,
-    'line_width': 1,
-    'opacity': 1
-}
-
-# Moving average for 10 days line
-avg_10 = df.Close.rolling(window=10, min_periods=1).mean()
-Trace2 = {
-    'x': df.index,
-    'y': avg_10,
-    'type': 'scatter',
-    'mode': 'lines',
-    'line': {
-        'width': 1,
-        'color': 'purple',
-            },
-    'name': '10 Day'
-}
-
-# Moving average for 50 days line
-avg_50 = df.Close.rolling(window=50, min_periods=1).mean()
-Trace3 = {
-    'x': df.index,
-    'y': avg_50,
-    'type': 'scatter',
-    'mode': 'lines',
-    'line': {
-        'width': 1,
-        'color': 'Yellow',
-            },
-    'name': '50 Day'
-}
-# Moving average for 100 days line
-avg_100 = df.Close.rolling(window=100, min_periods=1).mean()
-Trace4 = {
-    'x': df.index,
-    'y': avg_100,
-    'type': 'scatter',
-    'mode': 'lines',
-    'line': {
-        'width': 1,
-        'color': 'turquoise',
-            },
-    'name': '100 Day'
-}
-
-# Moving average for 200 days line
-avg_200 = df.Close.rolling(window=200, min_periods=1).mean()
-Trace5 = {
-    'x': df.index,
-    'y': avg_200,
-    'type': 'scatter',
-    'mode': 'lines',
-    'line': {
-        'width': 1,
-        'color': 'white',
-            },
-    'name': '200 Day'
-}
-# Volume Bar Graph
-Trace6 = {
-    'x': df.index,
-    'y': df.Volume,
-    'type': 'bar',
-    'name': 'Trading Volume'
-}
-
-
 #------------------------------------------------------------------------------
 #   - Dash App -
 #------------------------------------------------------------------------------
@@ -106,6 +23,7 @@ app.layout = html.Div(
     dcc.Input(
         id='stockInput',
         type="text",
+        value="SPY",
         placeholder="",
         debounce=True),
 
@@ -119,12 +37,94 @@ app.layout = html.Div(
     [Input("stockInput", "value")])
 
 def display_stock(value):
+    stock = value
+    df = web.DataReader(str(stock), data_source='yahoo', start='01-01-2019')
+ # Main candle stick chart
+    Trace1 = {
+        'x': df.index,
+        'open': df.Open,
+        'close': df.Close,
+        'high': df.High,
+        'low': df.Low,
+        'type': 'candlestick',
+        'name': stock,
+        'showlegend':True,
+        'line_width': 1,
+        'opacity': 1
+    }
+
+# Moving average for 10 days line
+    avg_25 = df.Close.rolling(window=25, min_periods=1).mean()
+    Trace2 = {
+        'x': df.index,
+        'y': avg_25,
+        'type': 'scatter',
+        'mode': 'lines',
+        'line': {
+            'width': 1,
+            'color': 'purple',
+                },
+        'name': '25 Day'
+    }
+
+# Moving average for 50 days line
+    avg_50 = df.Close.rolling(window=50, min_periods=1).mean()
+    Trace3 = {
+        'x': df.index,
+        'y': avg_50,
+        'type': 'scatter',
+        'mode': 'lines',
+        'line': {
+            'width': 1,
+            'color': 'Yellow',
+                },
+        'name': '50 Day'
+    }
+# Moving average for 100 days line
+    avg_100 = df.Close.rolling(window=100, min_periods=1).mean()
+    Trace4 = {
+        'x': df.index,
+        'y': avg_100,
+        'type': 'scatter',
+        'mode': 'lines',
+        'line': {
+            'width': 1,
+            'color': 'turquoise',
+                },
+        'name': '100 Day'
+    }
+
+# Moving average for 200 days line
+    avg_200 = df.Close.rolling(window=200, min_periods=1).mean()
+    Trace5 = {
+        'x': df.index,
+        'y': avg_200,
+        'type': 'scatter',
+        'mode': 'lines',
+        'line': {
+            'width': 1,
+            'color': 'white',
+                },
+        'name': '200 Day'
+    }
+# Volume Bar Graph
+    Trace6 = {
+        'x': df.index,
+        'y': df.Volume,
+        'type': 'bar',
+        'name': 'Trading Volume'
+    }   
+    # Make 2 charts
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
             vertical_spacing=0.03, subplot_titles=(value, 'Volume'),
             row_width=[0.2, 0.7])
+    # Add layout options
     fig.update_layout(
         xaxis_rangeslider_visible=False,
-        template="plotly_dark")
+        template="plotly_dark",
+        height=850
+        )
+    # Add Traces to the figre
     fig.add_trace(Trace1, row=1, col=1)
     fig.add_trace(Trace2, row=1, col=1)
     fig.add_trace(Trace3, row=1, col=1)
